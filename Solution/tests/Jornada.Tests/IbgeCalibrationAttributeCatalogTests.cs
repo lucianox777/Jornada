23f940fc3673a5f43cd446ecaaf698b11016bb75
lucianox777 <lucianovianna.backup@gmail.com>
2026-09-10 00:01:01 -0300

    test(linkage): align IBGE mapping with official first-name and surname dimensions

--- Solution/tests/Jornada.Tests/IbgeCalibrationAttributeCatalogTests.cs
+++ Solution/tests/Jornada.Tests/IbgeCalibrationAttributeCatalogTests.cs
@@ -4,16 +4,31 @@ namespace Jornada.Tests;
 
 public sealed class IbgeCalibrationAttributeCatalogTests
 {
-    [TestCase(BlockingCandidateFeatureCatalog.NameFull)]
-    [TestCase(BlockingCandidateFeatureCatalog.NameFirst)]
-    [TestCase(BlockingCandidateFeatureCatalog.NameSurnames)]
-    [TestCase(BlockingCandidateFeatureCatalog.NameLast)]
-    public void Name_features_are_enriched_by_known_ibge_name_source(string feature)
+    [Test]
+    public void First_name_uses_ibge_first_name_statistics()
     {
-        Assert.That(IbgeCalibrationAttributeCatalog.TryGetSource(feature, out var source), Is.True);
-        Assert.That(source, Is.EqualTo(ExternalNameFrequencyCatalog.IbgeSource));
+        Assert.That(IbgeCalibrationAttributeCatalog.TryGetMapping(
+            BlockingCandidateFeatureCatalog.FirstName, out var mapping), Is.True);
+        Assert.Multiple(() =>
+        {
+            Assert.That(mapping.Source, Is.EqualTo(ExternalNameFrequencyCatalog.IbgeSource));
+            Assert.That(mapping.StatisticKind, Is.EqualTo(IbgeNameStatisticKind.FirstName));
+        });
     }
 
+    [TestCase(BlockingCandidateFeatureCatalog.Surnames)]
+    [TestCase(BlockingCandidateFeatureCatalog.LastName)]
+    public void Surname_features_use_ibge_surname_statistics(string feature)
+    {
+        Assert.That(IbgeCalibrationAttributeCatalog.TryGetMapping(feature, out var mapping), Is.True);
+        Assert.Multiple(() =>
+        {
+            Assert.That(mapping.Source, Is.EqualTo(ExternalNameFrequencyCatalog.IbgeSource));
+            Assert.That(mapping.StatisticKind, Is.EqualTo(IbgeNameStatisticKind.Surname));
+        });
+    }
+
+    [TestCase(BlockingCandidateFeatureCatalog.FullName)]
     [TestCase(BlockingCandidateFeatureCatalog.BirthDay)]
     [TestCase(BlockingCandidateFeatureCatalog.BirthMonth)]
     [TestCase(BlockingCandidateFeatureCatalog.BirthYear)]
@@ -27,7 +42,10 @@ public sealed class IbgeCalibrationAttributeCatalogTests
     public void Unsupported_feature_does_not_disappear_from_optimizer_candidate_catalog()
     {
         Assert.That(BlockingCandidateFeatureCatalog.RequiredOptimizerCandidates,
+            Does.Contain(BlockingCandidateFeatureCatalog.FullName));
+        Assert.That(BlockingCandidateFeatureCatalog.RequiredOptimizerCandidates,
             Does.Contain(BlockingCandidateFeatureCatalog.BirthYear));
+        Assert.That(IbgeCalibrationAttributeCatalog.Supports(BlockingCandidateFeatureCatalog.FullName), Is.False);
         Assert.That(IbgeCalibrationAttributeCatalog.Supports(BlockingCandidateFeatureCatalog.BirthYear), Is.False);
     }
 }
