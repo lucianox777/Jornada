5f99be79083fc764ca4ba86e479bc3923494238d
lucianox777 <lucianovianna.backup@gmail.com>
2026-09-11 00:45:28 -0300

    test(calibrador): fixar colunas por versão de algoritmo

--- Solution/tests/Jornada.Tests/ResolutionProjectionModelCatalogTests.cs
+++ Solution/tests/Jornada.Tests/ResolutionProjectionModelCatalogTests.cs
@@ -49,7 +49,7 @@ public sealed class ResolutionProjectionModelCatalogTests
             Assert.That(calculated.Select(static x => x.Feature), Does.Contain("apelido_social__last"));
             Assert.That(calculated.All(static x => x.Origin == ResolutionFeatureOrigin.Calculated), Is.True);
             Assert.That(calculated.All(static x => x.ResolutionModel is not null), Is.True);
-            Assert.That(calculated.All(static x => x.Algorithm is not null), Is.True);
+            Assert.That(calculated.All(static x => x.Algorithm == "PERSON_NAME_COMPONENTS@V2"), Is.True);
         });
     }
 
@@ -73,22 +73,108 @@ public sealed class ResolutionProjectionModelCatalogTests
                 "data_evento__year"
             }));
             Assert.That(calculated.All(static x => x.Materialization == ResolutionMaterializationKind.GeneratedColumn), Is.True);
+            Assert.That(calculated.All(static x => x.Algorithm == "DATE_COMPONENTS@V2"), Is.True);
         });
     }
 
     [Test]
-    public void Build_SemanticWithoutHomologatedModelKeepsOnlyOriginalAttribute()
+    public void Build_ImplementedPhoneAndEmailAlgorithmsProduceCanonicalColumns()
     {
         var plan = ResolutionProjectionPlanner.Build(
-            new[] { new ResolutionSourceAttribute("email", ResolutionAttributeSemantic.Email) },
+            new[]
+            {
+                new ResolutionSourceAttribute("telefone_contato", ResolutionAttributeSemantic.Phone),
+                new ResolutionSourceAttribute("email_contato", ResolutionAttributeSemantic.Email)
+            },
+            "TEST_V1");
+
+        var calculated = plan.Features
+            .Where(static feature => feature.Origin == ResolutionFeatureOrigin.Calculated)
+            .ToArray();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(calculated.Select(static x => x.Feature), Is.EquivalentTo(new[]
+            {
+                "telefone_contato__canonical",
+                "email_contato__canonical"
+            }));
+            Assert.That(calculated.Single(static x => x.Feature == "telefone_contato__canonical").Algorithm,
+                Is.EqualTo("TELEFONE_BR_CANONICO@V2"));
+            Assert.That(calculated.Single(static x => x.Feature == "email_contato__canonical").Algorithm,
+                Is.EqualTo("EMAIL_CANONICO@V2"));
+        });
+    }
+
+    [Test]
+    public void Build_SemanticWithoutHomologatedAlgorithmKeepsOnlyOriginalAttribute()
+    {
+        var plan = ResolutionProjectionPlanner.Build(
+            new[] { new ResolutionSourceAttribute("logradouro", ResolutionAttributeSemantic.Address) },
             "TEST_V1");
 
         Assert.Multiple(() =>
         {
             Assert.That(plan.Features, Has.Count.EqualTo(1));
-            Assert.That(plan.Features[0].Feature, Is.EqualTo("source__email"));
+            Assert.That(plan.Features[0].Feature, Is.EqualTo("source__logradouro"));
             Assert.That(plan.Features[0].Origin, Is.EqualTo(ResolutionFeatureOrigin.Original));
             Assert.That(plan.BlockingCandidateFeatures, Is.Empty);
+        });
+    }
+
+    [Test]
+    public void AlgorithmVersion_FixesExactOutputColumnsAndRequiresImplementationAndTests()
+    {
+        Assert.That(
+            HomologatedResolutionAlgorithmCatalog.TryGet(
+                HomologatedResolutionAlgorithmCatalog.PersonNameComponentsAlgorithm,
+                HomologatedResolutionAlgorithmCatalog.PersonNameComponentsVersion,
+                out var nameAlgorithm),
+            Is.True);
+        Assert.That(
+            HomologatedResolutionAlgorithmCatalog.TryGet(
+                HomologatedResolutionAlgorithmCatalog.BrazilianPhoneCanonicalAlgorithm,
+                HomologatedResolutionAlgorithmCatalog.BrazilianPhoneCanonicalVersion,
+                out var phoneAlgorithm),
+            Is.True);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(nameAlgorithm.OutputColumns.Select(static x => x.CanonicalCode), Is.EqualTo(new[]
+            {
+                "normalized",
+                "first",
+                "surnames",
+                "last"
+            }));
+            Assert.That(phoneAlgorithm.OutputColumns.Select(static x => x.CanonicalCode), Is.EqualTo(new[] { "canonical" }));
+            Assert.That(nameAlgorithm.ImplementationReferences, Is.Not.Empty);
+            Assert.That(nameAlgorithm.TestReferences, Is.Not.Empty);
+            Assert.That(phoneAlgorithm.ImplementationReferences, Is.Not.Empty);
+            Assert.That(phoneAlgorithm.TestReferences, Is.Not.Empty);
+        });
+    }
+
+    [Test]
+    public void Catalog_AllowsSeveralAlgorithmsPerSemanticWithoutOneToOneDictionaryConstraint()
+    {
+        var personName = HomologatedResolutionAlgorithmCatalog.ForSemantic(ResolutionAttributeSemantic.PersonName);
+        var phone = HomologatedResolutionAlgorithmCatalog.ForSemantic(ResolutionAttributeSemantic.Phone);
+        var email = HomologatedResolutionAlgorithmCatalog.ForSemantic(ResolutionAttributeSemantic.Email);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(personName, Is.Not.Empty);
+            Assert.That(phone.Select(static x => x.QualifiedAlgorithm), Does.Contain("TELEFONE_BR_CANONICO@V2"));
+            Assert.That(email.Select(static x => x.QualifiedAlgorithm), Does.Contain("EMAIL_CANONICO@V2"));
+            Assert.That(HomologatedResolutionAlgorithmCatalog.All.Select(static x => x.Semantic),
+                Does.Contain(ResolutionAttributeSemantic.PersonName));
+            Assert.That(HomologatedResolutionAlgorithmCatalog.All.Select(static x => x.Semantic),
+                Does.Contain(ResolutionAttributeSemantic.Date));
+            Assert.That(HomologatedResolutionAlgorithmCatalog.All.Select(static x => x.Semantic),
+                Does.Contain(ResolutionAttributeSemantic.Phone));
+            Assert.That(HomologatedResolutionAlgorithmCatalog.All.Select(static x => x.Semantic),
+                Does.Contain(ResolutionAttributeSemantic.Email));
         });
     }
 
@@ -110,22 +196,6 @@ public sealed class ResolutionProjectionModelCatalogTests
             Assert.That(first.Fingerprint, Is.EqualTo(reordered.Fingerprint));
             Assert.That(first.Fingerprint, Is.Not.EqualTo(next.Fingerprint));
             Assert.That(first.Fingerprint, Has.Length.EqualTo(64));
-        });
-    }
-
-    [Test]
-    public void CatalogContainsOnlyExplicitlyHomologatedModels()
-    {
-        Assert.Multiple(() =>
-        {
-            Assert.That(HomologatedResolutionModelCatalog.All, Is.Not.Empty);
-            Assert.That(
-                HomologatedResolutionModelCatalog.All.Select(static model => model.Semantic),
-                Is.EquivalentTo(new[]
-                {
-                    ResolutionAttributeSemantic.PersonName,
-                    ResolutionAttributeSemantic.Date
-                }));
         });
     }
 }
