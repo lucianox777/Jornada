b9c0647b06f63b9e418f270b627f17f7a4a293fa
lucianox777 <lucianovianna.backup@gmail.com>
2026-09-17 23:37:28 -0300

    feat(linkage): persiste proveniência do gate de llr

--- Solution/src/Jornada.Linkage.Parameters.Worker/LinkageParameterEstimator.cs
+++ Solution/src/Jornada.Linkage.Parameters.Worker/LinkageParameterEstimator.cs
@@ -66,6 +66,7 @@ public static class LinkageParameterEstimator
             result[LinkageParameterCatalog.LogOddsConflictMargin] = conflictMargin;
             result[LinkageParameterCatalog.DecisionEvidenceScoring] = 1m;
             result[LinkageParameterCatalog.DualThresholdConflictGuard] = 1m;
+            result[LinkageParameterCatalog.OrderedNameLlrMonotonicity] = 1m;
         }
 
         AddDistribution(result, "M_NOME", matchedNameStates, smoothingAlpha);
