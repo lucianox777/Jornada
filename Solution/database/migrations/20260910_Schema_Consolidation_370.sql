dce22edb7259f3dc844d411994e372d79a72c489
lucianox777 <lucianovianna.backup@gmail.com>
2026-09-20 17:14:43 -0300

    build(schema): require model promotion ledger in 3.70

--- Solution/database/migrations/20260910_Schema_Consolidation_370.sql
+++ Solution/database/migrations/20260910_Schema_Consolidation_370.sql
@@ -22,6 +22,7 @@ FROM (VALUES
  (N'identidade.composicao_recomposicao_plano'),
  (N'identidade.composicao_publicacao'),
  (N'auditoria.decisao_identidade_evento'),
+ (N'auditoria.modelo_linkage_estado_evento'),
  (N'identidade.blocking_chave'),
  (N'identidade.linkage_ruleset'),
  (N'identidade.linkage_ruleset_passe'),
@@ -46,7 +47,8 @@ FROM (VALUES
  (N'serving.v_bi_completude_pessoa'),
  (N'serving.v_pessoa_nome_referencia'),
  (N'serving.v_pessoa'),
- (N'auditoria.v_decisao_identidade_evento')
+ (N'auditoria.v_decisao_identidade_evento'),
+ (N'auditoria.v_modelo_linkage_estado_evento')
 ) v(objeto)
 WHERE OBJECT_ID(v.objeto, N'V') IS NULL;
 
@@ -63,7 +65,9 @@ FROM (VALUES
  (N'identidade.tr_modelo_linkage_promotion_contract'),
  (N'identidade.tr_linkage_resultado_publicacao_imutavel'),
  (N'identidade.tr_linkage_resultado_bloqueia_delete'),
- (N'auditoria.tr_decisao_identidade_evento_append_only')
+ (N'auditoria.tr_decisao_identidade_evento_append_only'),
+ (N'auditoria.tr_modelo_linkage_estado_evento_append_only'),
+ (N'identidade.tr_modelo_linkage_estado_evento')
 ) v(objeto)
 WHERE OBJECT_ID(v.objeto, N'TR') IS NULL;
 
@@ -73,6 +77,8 @@ IF OBJECT_ID(N'identidade.sp_publicar_resolucao_progressiva_linkage',N'P') IS NU
     INSERT @missing(item) VALUES(N'PROC:identidade.sp_publicar_resolucao_progressiva_linkage');
 IF OBJECT_ID(N'auditoria.sp_registrar_decisao_identidade',N'P') IS NULL
     INSERT @missing(item) VALUES(N'PROC:auditoria.sp_registrar_decisao_identidade');
+IF NOT EXISTS(SELECT 1 FROM sys.indexes WHERE object_id=OBJECT_ID(N'auditoria.modelo_linkage_estado_evento') AND name=N'IX_modelo_linkage_estado_evento_modelo')
+    INSERT @missing(item) VALUES(N'INDEX:auditoria.modelo_linkage_estado_evento.IX_modelo_linkage_estado_evento_modelo');
 
 DECLARE @required_columns TABLE(tabela SYSNAME NOT NULL,coluna SYSNAME NOT NULL,PRIMARY KEY(tabela,coluna));
 INSERT @required_columns(tabela,coluna) VALUES
