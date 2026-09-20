b9b9cbddc49bb13da3c16ef279ef5eef0056569c
lucianox777 <lucianovianna.backup@gmail.com>
2026-09-20 00:00:33 -0300

    refactor(linkage): expose core internals to runner calibrator and tests

---
+++ Solution/src/Jornada.Linkage.Core/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Jornada.Linkage.Runner")]
+[assembly: InternalsVisibleTo("Jornada.Linkage.Parameters.Worker")]
+[assembly: InternalsVisibleTo("Jornada.Tests")]
