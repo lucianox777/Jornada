bc4453d1c288dd57761070d8943a6ea13a67770c
lucianox777 <lucianovianna.backup@gmail.com>
2026-09-20 19:36:57 -0300

    build(linkage): expose core internals only to conference orchestrator

--- Solution/src/Jornada.Linkage.Core/AssemblyInfo.cs
+++ Solution/src/Jornada.Linkage.Core/AssemblyInfo.cs
@@ -4,3 +4,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Jornada.Linkage.Parameters.Worker")]
 [assembly: InternalsVisibleTo("Jornada.Tests")]
 [assembly: InternalsVisibleTo("Jornada.Integration.Tests")]
+[assembly: InternalsVisibleTo("Jornada.Linkage.Conference")]
