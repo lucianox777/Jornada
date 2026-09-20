d282857283018bb2fd03693156fced328fbcc7f5
lucianox777 <lucianovianna.backup@gmail.com>
2026-09-20 03:49:23 -0300

    build(schema): regenerate v3.70 installer include list

--- Solution/database/Jornada_Fase1_v3.70.sql
+++ Solution/database/Jornada_Fase1_v3.70.sql
@@ -40,4 +40,5 @@
 :r database/migrations/20260919_Linkage_Publicacao_Progressiva.sql
 :r database/migrations/20260919_Gold_Pessoa_Progressiva.sql
 :r database/migrations/20260920_Linkage_Llr_Monotonicity_Tolerance.sql
+:r database/migrations/20260920_Linkage_Conflito_Revisao_Governada.sql
 :r database/migrations/20260910_Schema_Consolidation_370.sql
