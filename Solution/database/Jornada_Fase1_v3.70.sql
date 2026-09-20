0018220041ba2038c185f1f3f07b0850ba20b94c
lucianox777 <lucianovianna.backup@gmail.com>
2026-09-20 19:43:59 -0300

    build(schema): compose conference command governance migration

--- Solution/database/Jornada_Fase1_v3.70.sql
+++ Solution/database/Jornada_Fase1_v3.70.sql
@@ -45,4 +45,5 @@
 :r database/migrations/20260920_Identidade_Decisao_Ledger.sql
 :r database/migrations/20260920_Linkage_Model_Promotion_Ledger.sql
 :r database/migrations/20260920_Linkage_Implementation_Conference_Evidence.sql
+:r database/migrations/20260920_Linkage_Conference_Command_Governance.sql
 :r database/migrations/20260910_Schema_Consolidation_370.sql
